1d5eac578e5a47dc0e7f2ae2e64d0795e9f56d5e
flowedback <bjoern.abt@psi.ch>
2015-11-06 14:42:29 +0100

    change layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Halcyonic by HTML5 UP
+	Strongly Typed by HTML5 UP
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -22,8 +22,15 @@
 
 	$(function() {
 
-		var $window = $(window),
+		var	$window = $(window),
 			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				$body.removeClass('is-loading');
+			});
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
@@ -36,13 +43,24 @@
 				);
 			});
 
+		// CSS polyfills (IE<9).
+			if (skel.vars.IEVersion < 9)
+				$(':last-child').addClass('last-child');
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+				hoverDelay: 150,
+				hideDelay: 350
+			});
+
 		// Off-Canvas Navigation.
 
 			// Title Bar.
 				$(
 					'<div id="titleBar">' +
 						'<a href="#navPanel" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
 					'</div>'
 				)
 					.appendTo($body);
